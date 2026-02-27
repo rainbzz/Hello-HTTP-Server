1d56116a719d67aa7aa1bb5b8254bfcd6a300622
rainbzz <rainrein7@gmail.com>
2026-02-27 09:16:47 +0900

    introduction to server-side GET

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// write your code here
+const http = require('http');
+
+// create HTTP server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+// listen on port 8080
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
